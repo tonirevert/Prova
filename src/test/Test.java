47739aee13e0881b9d52de0dcae01ea7facdd7a4
antonio <antonio@antonio-LIFEBOOK-AH512>
2016-05-07 20:39:04 +0200

    First commit

---
+++ src/test/Test.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author antonio
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Test Java");
+    }
+    
+}
